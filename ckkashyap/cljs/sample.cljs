eebc43fe9b975f39e4c47070d8f61813ae21fb0b
CK Kashyap <ckkashyap@gmail.com>
2018-11-10 17:01:47 -0800

    all good

--- ckkashyap/cljs/sample.cljs
+++ ckkashyap/cljs/sample.cljs
@@ -1,4 +1,12 @@
-(ns sample)
+(ns sample
+  (:require
+   [clojure.string :as string]
+   [goog.html.SafeHtml :as goto.html.SafeHtml]
+   [goog.events :as goog.events]
+   [goog.object :as goog.object]
+   [goog.net.XhrIo :as xhr]
+   [goog.json :as gjson]
+   [goog.dom :as goog.dom]))
 
 (println "hello world")
 
