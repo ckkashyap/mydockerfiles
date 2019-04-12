4fc31587072e1c70725f54485985095c7500efba
CK Kashyap <ckkashyap@gmail.com>
2019-04-11 23:05:39 -0700

    working version with pil

--- ckkashyap/pil/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-service ssh start
-# HELLO WORLD
-apt-get install -y python3
-python3 -m http.server 80
--- ckkashyap/pil/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd /root
-wget --no-check-certificate https://$NGROK/script.sh
-chmod +x script.sh
-./script.sh
